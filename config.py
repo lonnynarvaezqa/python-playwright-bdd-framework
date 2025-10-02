4f01549801387c7b167dc869e876f7d3ba5cb144
Lonny Narváez <lonny@MacBook-Air-de-Lonny.local>
2025-10-02 13:41:14 -0600

    update readme

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+class AppConfig:
+    """Clase para almacenar URLs y constantes de la aplicación."""
+
+    # URL Base de la aplicación de prueba
+    BASE_URL = "https://the-internet.herokuapp.com"
+
+    # Paths específicos de la aplicación
+    LOGIN_PATH = "/login"
+    UPLOAD_PATH = "/upload"
+    HOME_PATH = "/"
